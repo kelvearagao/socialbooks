081fc20f05a3a71b8c7bf013ce31bb1f0cb4d25a
Kelve <kelvearagao@gmail.com>
2016-10-01 20:10:50 -0300

    Validando as entradas na nossa API

--- src/main/java/com/kelvearagao/socialbooks/resources/AutoresResource.java
+++ src/main/java/com/kelvearagao/socialbooks/resources/AutoresResource.java
@@ -3,6 +3,8 @@ package com.kelvearagao.socialbooks.resources;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,7 +42,7 @@ public class AutoresResource {
 	 * @return
 	 */
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> salvar(@RequestBody Autor autor) {
+	public ResponseEntity<Void> salvar(@Valid @RequestBody Autor autor) {
 		autor = autoresService.salvar(autor);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(autor.getId()).toUri();
